27c9c305e9efb816747e7cd35fee125468011fd6
darrylcousins <darryljcousins@gmail.com>
2019-08-11 12:20:35 +1200

    more fonts and layout

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
       <?php if ( !is_front_page() ) : ?>
-          <?php the_title( '<h1 class="">', '</h1>' ); ?>
+          <?php the_title( '<h1 class="ui header huge dividing">', '</h1>' ); ?>
       <?php endif; ?>
     </header><!-- .entry-header -->
 
